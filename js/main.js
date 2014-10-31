b3533985f1479394e2cf960d27d9eab611ef0e90
Ryan Janvier <ryan@liquidtrends.com>
2014-10-31 11:33:09 -0700

    Add regex to array

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
+$(document).ready(function() 
+    { 
+        $("#mainTable").tablesorter(); 
+    } 
+); 
+
 function analyzeText(){
   var showText=document.getElementById('output');
   var getText = document.getElementById('textInput');
-  var split = getText.value.split(' ');
+  var split = getText.value.trim(' ').replace(/[^A-z\']+/g," ").split(' ');
   var label = ('<span class="label label-success">Success</span>');
 
 if (getText.value != "") {
 
   showText.innerHTML="You Wrote: " + getText.value;
   for (var i=0; i< split.length; i++) {
-    $("table").append("<tr><td> " + split[i] + "</td><td>" + split[i].length + "</td><td>" + label + "</td></tr>");
+    $("table").append("<tr><td> " + split[i].toLowerCase() + "</td><td>" + split[i].length + "</td><td>" + label + "</td></tr>");
   }
 }
 else if (getText.value == "") {
